9d2737d613737fb3d984b8d01b93b2ec9d4fd18a
dashachernyh <dasha.chernyh.@mail.ru>
2020-02-21 19:03:03 +0300

    ggggg

--- Project17/Project17/List_head.h
+++ Project17/Project17/List_head.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <iostream>
+using namespace std;
+struct TMonomom
+{
+	double coeff;
+	int px, py, pz;
+	bool operator <(TMonomom &m)
+	{
+		if ((px < m.px)||(px==m.px && py<m.py)||(px==m.px && py==m.py && pz<m.pz)) return true;
+	    else return false;
+	}
+	bool operator ==(TMonomom &m)
+	{
+		if (px == m.px && py == m.py && pz == m.pz) return true;
+		else return false;
+	}
+	bool operator >(TMonomom &m)
+	{
+		if((px>m.px)||(px==m.px && py>m.py)||(px==m.px && py==m.py && pz>m.pz)) return true;
+		else return false;
+	}
+
+};
